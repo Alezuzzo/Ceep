8d4f6c908f9725c5e863d53329ab27cbd5831988
Alezuzzo <Thallesander@hotmail.com>
2021-04-20 14:45:35 -0300

    adding components

---
+++ src/components/ListaDeNotas.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+export class ListaDeNotas extends Component {
+  render() {
+    return (
+      <ul>
+        <li>
+          <section>
+            <header>
+              <h3>Título</h3>
+            </header>
+          </section>
+        </li>
+      </ul>
+    );
+  }
+}
